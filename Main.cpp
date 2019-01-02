b8bf555dc2976d9d52a45381028b674e9ffd2f76
Илья Колдунов <gigafiga21@gmail.com>
2019-01-02 22:00:45 +0300

    Created test file (issue #2).

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,21 @@
 #include <ncurses.h>
-#include "Lbraries/Menu/menu.h"
+#include "Lbraries/Interface/interface.h"
 
 int main()
 {
+    initscr();
+
     Menu menu(5, 5, 10, 10);
     menu.addButton("Start");
     menu.addButton("Continue");
     menu.addButton("Stop");
     menu.addButton("Quit");
-
-    initscr();                   // Переход в curses-режим
-
     menu.draw();
 
-    refresh();                   // Вывод приветствия на настоящий экран
-    getch();
-    endwin();                    // Выход из curses-режима. Обязательная команда.
+    Navigator navigator;
+    navigator.pushUnit(&menu, 4);
+    navigator.listen();
+    refresh();
+    endwin();
     return 0;
 }
