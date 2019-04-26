5569ee66df09da08d8557946057fcf6d0e96c5ec
gigafiga21 <gigafiga21@gmail.com>
2019-04-26 09:47:22 +0300

    Added event dispatching methods to Widget (issue #10).

--- Libraries/Interface/interface.cpp
+++ Libraries/Interface/interface.cpp
@@ -42,3 +42,37 @@ void Widget::units(short amount)
 {
     _units = amount;
 }
+
+/**
+ * Checks if event is dispatchable
+ * @param  {unsigned char} name - name of the event
+ * @return {bool}               - true if event listeners are existing and active
+ */
+bool Widget::event(unsigned char name)
+{
+    return events & name;
+}
+
+/**
+ * Setts listener to the event
+ * @param {unsigned char} name     - name of the event
+ * @param {Listener}      function - event listener function
+ */
+void Widget::listener(unsigned char name, Listener function)
+{
+    listeners[name].push_back(function);
+    events = events | name;
+}
+
+/**
+ * Dispatches event on widget
+ * @param {unsigned char} name        - name of the event
+ * @param {Event}         information - struct passed into called listeners
+ */
+void Widget::dispatch(unsigned char name, Event information)
+{
+    for (std::list<Listener>::iterator function = listeners[name].begin(); function != listeners[name].end(); function++)
+    {
+        (*function)(information);
+    }
+}
--- Libraries/Interface/interface.h
+++ Libraries/Interface/interface.h
@@ -1,15 +1,42 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
+#include <functional>
+#include <map>
+#include <list>
+
+/**
+ * Arguments for events methods
+ * @const {unsigned char} EVENT_FOCUS   - event of focusing
+ * @const {unsigned char} EVENT_UNFOCUS - event of unfocusing
+ * @const {unsigned char} EVENT_ACTIVE  - event of activating widget
+ */
+#define EVENT_FOCUS   1
+#define EVENT_UNFOCUS 2
+#define EVENT_ACTIVE  4
+
+typedef struct
+{
+    short unit;
+} Event;
+
+/**
+ * Describes method called by event dispatching
+ * @typedef {std::function<void()>} listener
+ */
+typedef std::function<void(Event)> Listener;
 
 /**
  * Basic class of the console widget (to store different widgets in one list)
- * @prop {Widget*} _next     - next widget in the object model
- * @prop {Widget*} _previous - previous widget in the object model
+ * @prop {short}                                        _units    - amount of non-widget children
+ * @prop {unsigned char}                                events    - binary store for events could be dispatched at current widget
+ * @prop {std::map<unsigned char, std::list<Listener>>} listeners - store for event listeners
  */
 class Widget
 {
     public:
         short _units;
+        unsigned char events;
+        std::map<unsigned char, std::list<Listener>> listeners;
 
     public:
         virtual void focus(int index);
@@ -17,6 +44,9 @@ class Widget
         virtual void draw();
         short units();
         void units(short amount);
+        bool event(unsigned char name);
+        void listener(unsigned char name, Listener function);
+        void dispatch(unsigned char name, Event information);
 };
 
 #endif
