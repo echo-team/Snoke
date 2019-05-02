338df04f1554fabe24a5cc676e02ffa58d894433
Travis CI <travis@travis-ci.org>
2019-05-02 15:25:52 +0000

    Deploy code docs to GitHub Pages Travis build: 3
    Commit: 5c09c06110ac077a1cb14624ee568b8fb93c2a3b

---
+++ html/annotated.js
@@ -0,0 +1,18 @@
+var annotated =
+[
+    [ "_Point", "struct___point.html", "struct___point" ],
+    [ "_PointStyle", "struct___point_style.html", "struct___point_style" ],
+    [ "Ball", "class_ball.html", "class_ball" ],
+    [ "Game", "class_game.html", "class_game" ],
+    [ "Labyrinth", "class_labyrinth.html", "class_labyrinth" ],
+    [ "Logo", "class_logo.html", "class_logo" ],
+    [ "MainMenu", "class_main_menu.html", "class_main_menu" ],
+    [ "Menu", "class_menu.html", "class_menu" ],
+    [ "MenuButton", "struct_menu_button.html", "struct_menu_button" ],
+    [ "NavigateUnit", "struct_navigate_unit.html", "struct_navigate_unit" ],
+    [ "Navigator", "class_navigator.html", "class_navigator" ],
+    [ "SideButton", "struct_side_button.html", "struct_side_button" ],
+    [ "SideMenu", "class_side_menu.html", "class_side_menu" ],
+    [ "Snake", "class_snake.html", "class_snake" ],
+    [ "Widget", "class_widget.html", "class_widget" ]
+];
