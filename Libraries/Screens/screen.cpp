45e81203e3c1d5b31278e17fc70c155a4c1a1542
gigafiga21 <gigafiga21@gmail.com>
2019-05-10 01:13:49 +0300

    Added extra-documentation for the focusable widget search method (issue #10).

--- Libraries/Screens/screen.cpp
+++ Libraries/Screens/screen.cpp
@@ -23,11 +23,21 @@ Widget* Screen::first()
  */
 Widget* Screen::next(Widget* start)
 {
+    /**
+     * @var {Widget*} current - start point for searching focusable widget
+     * @var {bool}    diving  - flag for immersion into children of widget (false if there are no children and need go up)
+     */
     Widget* current = start;
-    char diving = true;
+    bool diving = true;
 
     do
     {
+        /**
+         * There are 2 modes - immersion (checking picked widgets) and bubbling throuch widgets were already checked (diving == false)
+         * If there are children - check them.
+         * If there are no children, but next sibling exists - check it
+         * Else - bubble to the last widget with sibling
+         */
         if (diving && current->firstChild())
         {
             current = current->firstChild();
@@ -43,6 +53,10 @@ Widget* Screen::next(Widget* start)
             diving = false;
         }
 
+        /**
+         * If listeners for focus event are binded - stop search
+         * If the top widget reached - start diving again
+         */
         if (diving && current->event(EVENT_FOCUS))
         {
             return current;
@@ -64,11 +78,20 @@ Widget* Screen::next(Widget* start)
  */
 Widget* Screen::previous(Widget* start)
 {
+    /**
+     * @var {Widget*} current - start point for searching focusable widget
+     * @var {bool}    bubling - flag for bubbling to the parent of widget (false if previous siblings of widget exist)
+     */
     Widget* current = start;
     char bubbling = true;
 
     do
     {
+        /**
+         * There are 2 modes - bubbling (checking picked widgets) and diving to the end of widget treen brunch (diving == false)
+         * If it is possible and bubbling mode on - go to the previous sibling (with no child) or to the parent (if all children were checked)
+         * Else - dive to the childfree subchild of the previous widget
+         */
         if (bubbling && current->previous())
         {
             current = current->previous();
@@ -90,6 +113,10 @@ Widget* Screen::previous(Widget* start)
             }
         }
 
+        /**
+         * If it is bubbling now and it is possible to dispatch focus event - stop search
+         * If the top widget reached - enable diving mode
+         */
         if (bubbling && current->event(EVENT_FOCUS))
         {
             return current;
