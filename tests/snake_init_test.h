96536141719f07c15ddd22038d99fe7d4ce10634
hemmemm <bkbk.78@bk.ru>
2019-05-10 22:20:54 +0300

    additional tests for Snake::init(issue #11)

--- tests/snake_init_test.h
+++ tests/snake_init_test.h
@@ -86,5 +86,86 @@ TEST(snake_init_test, test3) {
     }
 }
 
+/**
+ * @brief negative test for 1-st of the staring Point being outside the valid area
+ */
+TEST(snake_init_test, test4) {
+
+    /*
+     * Minimal environment block for the snake_init_test
+     */
+    Snake snake;
+    Point begin;
+    begin.x = -1;
+    begin.y = 1;
+    short direction = MVLEFT;
+    int length = 5;
+
+    bool retVal = snake.init(begin, direction, length);
+
+    if(!retVal)
+    {
+        SUCCEED();
+    }
+    else
+    {
+        FAIL();
+    }
+}
+
+/**
+ * @brief negative test for the 2-nd coordinate of the staring Point being outside the valid area
+ */
+TEST(snake_init_test, test5) {
+
+    /*
+     * Minimal environment block for the snake_init_test
+     */
+    Snake snake;
+    Point begin;
+    begin.x = 1;
+    begin.y = -1;
+    short direction = MVLEFT;
+    int length = 5;
+
+    bool retVal = snake.init(begin, direction, length);
+
+    if(!retVal)
+    {
+        SUCCEED();
+    }
+    else
+    {
+        FAIL();
+    }
+}
+
+/**
+ * @brief negative test for both coordinates of the staring Point being outside the valid area
+ */
+TEST(snake_init_test, test6) {
+
+    /*
+     * Minimal environment block for the snake_init_test
+     */
+    Snake snake;
+    Point begin;
+    begin.x = -1;
+    begin.y = -1;
+    short direction = MVLEFT;
+    int length = 5;
+
+    bool retVal = snake.init(begin, direction, length);
+
+    if(!retVal)
+    {
+        SUCCEED();
+    }
+    else
+    {
+        FAIL();
+    }
+}
+
 
 #endif // SNAKE_INIT_TEST_H
