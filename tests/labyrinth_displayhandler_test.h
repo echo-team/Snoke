64adc95093c78210c0a87199609e444c04ec5221
hemmemm <bkbk.78@bk.ru>
2019-05-15 05:41:50 +0300

    Added some more tests for Labyrinth::displayHandler(issue #11)

--- tests/labyrinth_displayhandler_test.h
+++ tests/labyrinth_displayhandler_test.h
@@ -129,4 +129,78 @@ TEST(labyrinth_displayHandler_test, test3)
     SUCCEED();
 }
 
+TEST(labyrinth_displayHandler_test, test4)
+{
+    initscr();
+    gameFieldSize.x = 40;
+    gameFieldSize.y = 20;
+    Labyrinth labyrinth;
+
+    bool retVal = labyrinth.setLabyrinth(gameFieldSize);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    Snake snake;
+    Point begin;
+    begin.x = 1;
+    begin.y = 1;
+    short direction = MVRIGHT;
+    int length = 5;
+
+    retVal = snake.init(begin, direction, length);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    retVal = labyrinth.addSnake(&snake);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    labyrinth.displayHandler(DISPFULL);
+    endwin();
+    SUCCEED();
+}
+
+TEST(labyrinth_displayHandler_test, test5)
+{
+    initscr();
+    gameFieldSize.x = 40;
+    gameFieldSize.y = 20;
+    Labyrinth labyrinth;
+
+    bool retVal = labyrinth.setLabyrinth(gameFieldSize);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    Snake snake;
+    Point begin;
+    begin.x = 1;
+    begin.y = 1;
+    short direction = MVRIGHT;
+    int length = 5;
+
+    retVal = snake.init(begin, direction, length);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    retVal = labyrinth.addSnake(&snake);
+    if(!retVal)
+    {
+        FAIL();
+    }
+
+    labyrinth.displayHandler(DISPUPD);
+    endwin();
+    SUCCEED();
+}
+
 #endif // LABYRINTH_DISPLAYHANDLER_TEST_H
